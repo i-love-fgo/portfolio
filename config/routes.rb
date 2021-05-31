4a2fecc870546e14ffdc9aaed03c3f09bebe196c
i-love-fgo <79355329+i-love-fgo@users.noreply.github.com>
2021-05-31 18:06:00 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root to: 'introductions#index'
   resources :introductions, only: [:index, :show]
-  resources :products, only: :index
+  resources :products, only: [:index, :show]
 end
